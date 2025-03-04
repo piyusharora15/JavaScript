035e54b22ccfdcc213fb2b342d05d4f191b23f9f
Piyush Arora <114138186+piyusharora15@users.noreply.github.com>
2025-03-04 20:55:37 +0530

    Add files via upload

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,4 +1,4 @@
-const month = "mar";
+const month = "nov";
 switch (month) {
     case "jan":
         console.log("January");
@@ -11,6 +11,30 @@ switch (month) {
         break;
     case "apr":
         console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "june":
+        console.log("June");
+        break;
+    case "july":
+        console.log("July");
+        break;
+    case "aug":
+        console.log("August");
+        break;
+    case "sept":
+        console.log("September");
+        break;
+    case "oct":
+        console.log("October");
+        break;
+    case "nov":
+        console.log("November");
+        break;
+    case "dec":
+        console.log("December");
         break;
     default:
         console.log("Default case match");
