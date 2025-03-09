16e08030268df36a467b21d1855f79d0df5edfd4
Piyush Arora <114138186+piyusharora15@users.noreply.github.com>
2025-03-09 21:13:28 +0530

    Add files via upload

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -3,6 +3,7 @@ const arr = [1, 2, 3, 4, 5];
 for (const num of arr) {
     console.log(num);
 }
+// for-of loop displays values.
 const greetings = "Hello world!";
 for (const greet of greetings) {
     //console.log(`Each char is ${greet}`);
@@ -15,3 +16,15 @@ map.set('USA', "United States of America");
 map.set('FR', "France");
 map.set('IN', "India");// map stores only unique values and in the order in which they are entered.
 console.log(map);
+// Map is iterable
+for(const [key, value] of map){
+    console.log(key, ':-', value);
+}
+// Object is not iterable
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+// for(const [key, value] of myObject){
+//     console.log(key, ':-', value);
+// }
